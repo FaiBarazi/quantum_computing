57177986c4d62486b2aee788f3c3f59c3218d309
Faisal Barazi <FaiBarazi@users.noreply.github.com>
2025-06-26 16:24:41 -0600

    Create qrng metaclasses

---
+++ qrng.py
@@ -0,0 +1,25 @@
+import qutip
+from abc import ABC, ABCMeta, abstractmethod
+from contextlib import contextmanager
+
+class Qubit(metclass=ABCMeta):
+    
+
+
+class QuantumDevice(metaclass=ABCMeta):
+    @abstractmethod
+    def allocate_qubit(self) -> Qubit:
+        pass
+
+    @abstractmethod
+    def deallocate_qubit(self, qubit: Qubit):
+        pass
+
+    @contextmanager
+    def using_qubit(self):
+        qubit = self.allocate_qubit()
+        try:
+            yield qubit
+        finally:
+            qubit.reset()
+            self.deallocate_qubit(qubit)
