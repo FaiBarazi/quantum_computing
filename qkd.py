ff12460d48940a0ed21ad65961a880c789416253
Faisal Barazi <FaiBarazi@users.noreply.github.com>
2025-07-28 18:59:13 -0600

    Apply both axis qubit preparation and measure

--- qkd.py
+++ qkd.py
@@ -2,17 +2,22 @@ from interface import Qubit, QuantumDevice
 
 
 def prepare_classical_message(bit: bool, q: Qubit) -> None:
+    if bit:
+        q.x()
+
+def prepare_classical_message_plusminus(bit: bool, q: Qubit) -> None:
     """
     If classical bit is one, prepare a qubit in |0>
     else prepare |+> 
     """
-
     if bit:
         q.x()
     q.h()
 
-
 def target_measure(q: Qubit) -> bool:
+    return q.measure
+
+def target_measure_plusminus(q: Qubit) -> bool:
     q.h()
     return q.measure()
 
@@ -23,3 +28,9 @@ def send_classical_bit(device: QuantumDevice, bit: bool) -> None:
         result = target_measure(q)
         q.reset()
     assert result == bit
+
+def send_classical_bit_plusminus(device: QuantumDevice, bit: bool) -> None:
+    with device.using_qubit() as q:
+        prepare_classical_message_plusminus(bit, q)
+        result = target_measure_plusminus(q)
+        assert result == bit
